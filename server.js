601b02bad0eda153e19167904dab5e2c42c56895
BenWofford <woffordbt@gmail.com>
2021-12-14 13:55:51 -0500

    finished tweaking API routes

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
@@ -19,41 +18,81 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useFindAndModify: false,
 });
 
+// API Routes
+
+// GET most recent workout
+app.get("/api/workouts", (req, res) => {
+  Workout.aggregate([
+    { $sort: { _id: -1 } },
+    { $limit: 1 },
+    { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+  ])
+    .then((workouts) => {
+      res.json(workouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// PUT a new exercise in a Workout document's array
+app.put("/api/workouts/:id", (req, res) => {
+  let newExercise = req.body;
+  Workout.findOneAndUpdate(
+    { _id: req.params.id },
+    { $push: { exercises: newExercise } }
+  )
+    .then((requestedWorkout) => {
+      res.json(requestedWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// POST new workout
+app.post("/api/workouts", (req, res) => {
+  Workout.create({
+    day: new Date(new Date().setDate(new Date().getDate() - 9)),
+    exercises: req.data,
+  })
+    .then((newWorkout) => {
+      res.json(newWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// GET all workouts
+app.get("/api/workouts/range", (req, res) => {
+  Workout.aggregate([
+    { $match: { _id: { $exists: true } } },
+    { $addFields: { totalDuration: { $sum: "$exercises.duration" } } },
+  ])
+    .then((allWorkouts) => {
+      res.json(allWorkouts);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 // HTML Routes
+app.get("/exercise?id=:id", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/exercise.html"));
+});
+
 app.get("/exercise", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/exercise.html"));
+  res.sendFile(path.join(__dirname, "public/exercise.html"));
 });
 
 app.get("/stats", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/stats.html"));
-});
-
-app.get("/api/workouts", (req, res) => {
-  db.Workout.aggregate({}).then((workouts) => {
-    res.json(workouts);
-  });
-});
-
-app.put("/api/workouts", (req, res) => {
-  db.Workout.updateMany({}).then((workouts) => {
-    res.json(workouts);
-  });
-});
-
-app.post("/api/workouts", (req, res) => {
-  db.Workout.create({}).then((workouts) => {
-    res.json(workouts);
-  });
-});
-
-app.get("/api/workouts/range", (req, res) => {
-  db.Workout.aggregate({}).then((workouts) => {
-    res.json(workouts);
-  });
+  res.sendFile(path.join(__dirname, "public/stats.html"));
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "/public/index.html"));
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, () => {
